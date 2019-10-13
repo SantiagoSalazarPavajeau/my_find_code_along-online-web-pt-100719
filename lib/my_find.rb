2a0134f742fbe14d563456c5a151760aeafb3c34
Santiago Salazar <santiago.s.pavajeau@gmail.com>
2019-10-13 23:02:07 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -4,6 +4,8 @@ def my_find(collection)
 i = 0
   while i < collection.length
     return collection[i] if yield(collection[i])
+    # this is a one line refactored if statement modifier
+    # if there is such a number that comes out to be true on the block it will be returned
     i = i + 1
   end
 end
